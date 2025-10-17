5f7c34ee9a4daee3007f4ef195fa8cf23b87c717
bhety.tech <bhety.tech@gmail.com>
2025-10-17 13:13:41 +0200

    added cors options

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const app = express();
 
 
 const corsOptions = {
-    origin: "http://localhost:3001",
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3001", "http://localhost:3001", "https://movie-app-frontend-bhety-ps-projects.vercel.app"]
+
 }
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
